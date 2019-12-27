e1496a4a39b27f193353fa103e174bc67832f02d
gurgi414 <gurgithemastermind@gmail.com>
2019-12-26 23:54:06 -0600

    Started work on basic graphics for the game. Need to decide on a basic framework for it before I can move forward with anything

--- GM.py
+++ GM.py
@@ -4,7 +4,7 @@ from graphics import *
 
 class GM:
     def __init__(self):
-        self.message = "df";
+        self.message = "df"
 
     def say(self, message):
         self.message = message
@@ -12,12 +12,33 @@ class GM:
 
 
 def main():
-    gameMaster = GM()
-    gameMaster.say('hello world!')
-    initBoard = BS(
-        [True, False, False, False, False, False, False, False, False, False, False, False, False, False, False])
-    win = GraphWin()
+    activeColor = color_rgb(7,140,3)
+    emptyColor = color_rgb(242, 5, 5)
+    outlineColor = color_rgb(242, 226, 5)
+
+    rad = 20
+
+    print("test")
+    win = GraphWin("My Circle", 500, 500)
+    win.setBackground(color_rgb(207, 155, 242))
+
+    pt = Point(win.getHeight()/2,win.getWidth()/2)
+    c = Circle(pt, rad)
+    c.setFill(activeColor)
+    c.setOutline(outlineColor)
+    c.setWidth(10)
+
+    pt2 = Point(pt.getX(),pt.getY()+40)
+    c2 = Circle(pt2,rad)
+    c2.setFill(emptyColor)
+    c2.setOutline(outlineColor)
+    c2.setWidth(10)
+
+    c.draw(win)
+    c2.draw(win)
+
+    win.getMouse()
+    win.close()
 
 
-if __name__ == "__main__":
-    main()
+main()
